bef8a8da093d658e237b7e6291e6c7f86a7535af
scarlanathan <nathan.han@epitech.eu>
2025-03-17 15:52:58 +0100

    feat: update

--- backend/vite.config.mjs
+++ backend/vite.config.mjs
@@ -6,8 +6,5 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: './tests/setup.js'
-  },
-  build: {
-    outDir: 'dist', // Chemin de sortie par défaut
-  },
+  }
 });
